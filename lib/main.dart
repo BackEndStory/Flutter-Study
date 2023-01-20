6ee8dadf22fa1c2f7ff9cf406be5d2a48c27d750
seungryeol <opop0421@naver.com>
2023-01-21 01:52:04 +0900

    use WebView

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:helloworld/screen/home_screen.dart';
 
 void main() {
-  runApp(SpalshScreen());
-}
-
-class SpalshScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-       return MaterialApp(
-         debugShowCheckedModeBanner: false,
-           home: Scaffold(
-             body: Container(
-               decoration: BoxDecoration(
-                 color: Color(0xFFF99231),
-               ),
-               child:Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                   Column(
-                   mainAxisAlignment: MainAxisAlignment.center,
-                   children: [
-                     Image.asset('assets/logo.png',width: 200,
-                     ),
-                     CircularProgressIndicator(
-                       valueColor: AlwaysStoppedAnimation(
-                         Colors.white
-                       ),
-                     ),
-                   ],
-                   ),
-                 ],
-             ),
-           ),
-          ),
-       );
-  }
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:HomeScreen(),
+    )
+  );
 }
